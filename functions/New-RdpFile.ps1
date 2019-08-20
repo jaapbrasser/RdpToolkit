7e861ab15b1f3d32bffabceab1ad7db3622c3414
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-21 01:55:00 +0200

    Prepped comment-based help

--- functions/New-RdpFile.ps1
+++ functions/New-RdpFile.ps1
@@ -1,52 +1,100 @@
 function New-RdpFile {
     param(
         $Path,
+        # 
         $screen_mode_id,
+        # 
         $use_multimon,
+        # 
         $desktopwidth,
+        # 
         $desktopheight,
+        # 
         $session_bpp,
+        # 
         $winposstr,
+        # 
         $compression,
+        # 
         $keyboardhook,
+        # Indicates whether audio input/output redirection is enabled.
         $audiocapturemode,
+        # 
         $videoplaybackmode,
+        # 
         $connection_type,
+        # 
         $networkautodetect,
+        # 
         $bandwidthautodetect,
+        # 
         $displayconnectionbar,
+        # 
         $enableworkspacereconnect,
+        # 
         $disable_wallpaper,
+        # 
         $allow_font_smoothing,
+        # 
         $allow_desktop_composition,
+        # 
         $disable_full_window_drag,
+        # 
         $disable_menu_anims,
+        # 
         $disable_themes,
+        # 
         $disable_cursor_setting,
+        # 
         $bitmapcachepersistenable,
+        # 
         $full_address,
+        # 
         $audiomode,
+        # 
         $redirectprinters,
+        # 
         $redirectcomports,
+        # 
         $redirectsmartcards,
+        # 
         $redirectclipboard,
+        # 
         $redirectposdevices,
+        # 
         $autoreconnection_enabled,
+        # 
         $authentication_level,
+        # 
         $prompt_for_credentials,
+        # 
         $negotiate_security_layer,
+        # 
         $remoteapplicationmode,
+        # Determines whether a program starts automatically when you connect with RDP. To specify an alternate shell, enter a valid path to an executable file for the value, such as "C:\ProgramFiles\Office\word.exe". This setting also determines which path or alias of the Remote Application to be started at connection time if RemoteApplicationMode is enabled.
         $alternate_shell,
+        # 
         $shell_working_directory,
+        # 
         $gatewayhostname,
+        # 
         $gatewayusagemethod,
+        # 
         $gatewaycredentialssource,
+        # 
         $gatewayprofileusagemethod,
+        # 
         $promptcredentialonce,
+        # 
         $gatewaybrokeringtype,
+        # 
         $use_redirection_server_name,
+        # 
         $rdgiskdcproxy,
+        # 
         $kdcproxyname,
+        # 
         $drivestoredirect
+        # 
     )
 }
