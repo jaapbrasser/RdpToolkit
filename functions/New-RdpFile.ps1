bf5c6fe6c580c7af482864f17b4d708e1fd4efff
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-21 02:07:48 +0200

    Added more help and default values

--- functions/New-RdpFile.ps1
+++ functions/New-RdpFile.ps1
@@ -5,28 +5,33 @@ function New-RdpFile {
         $screen_mode_id,
         # 
         $use_multimon,
-        # 
-        $desktopwidth,
-        # 
-        $desktopheight,
+        # Determines the resolution width (in pixels) on the remote computer when you connect by using Remote Desktop Connection. This setting corresponds to the selection in the Display configuration slider on the Display tab under Options in RDC.
+        [validaterange(200,4096)]
+        [int] $desktopwidth = 1280,
+        # Determines the resolution height (in pixels) on the remote computer when you connect by using Remote Desktop Connection. This setting corresponds to the selection in the Display configuration slider on the Display tab under Options in RDC.
+        [validaterange(200,2048)]
+        [int] $desktopheight = 720,
         # 
         $session_bpp,
         # 
         $winposstr,
-        # 
-        $compression,
+        # Determines whether bulk compression is enabled when it is transmitted by RDP to the local computer. - 0: Disable RDP bulk compression - 1: Enable RDP bulk compression
+        [validaterange(0,1)]
+        [int] $compression = 1,
         # 
         $keyboardhook,
-        # Indicates whether audio input/output redirection is enabled.
-        $audiocapturemode,
+        # Indicates whether audio input/output redirection is enabled. - 0: Disable audio capture from the local device - 1: Enable audio capture from the local device and redirection to an audio application in the remote session
+        [validaterange(0,1)]
+        [int] $audiocapturemode = 0,
         # 
         $videoplaybackmode,
         # 
         $connection_type,
         # 
         $networkautodetect,
-        # 
-        $bandwidthautodetect,
+        # Determines whether automatic network type detection is enabled - 0: Disable automatic network type detection - 1: Enable automatic network type detection
+        [validaterange(0,1)]
+        [int] $bandwidthautodetect = 1,
         # 
         $displayconnectionbar,
         # 
@@ -49,8 +54,9 @@ function New-RdpFile {
         $bitmapcachepersistenable,
         # 
         $full_address,
-        # 
-        $audiomode,
+        # Determines whether the local or remote machine plays audio. - 0: Play sounds on local computer (Play on this computer) - 1: Play sounds on remote computer (Play on remote computer) - 2: Do not play sounds (Do not play)
+        [validaterange(0,2)]
+        [int] $audiomode = 0,
         # 
         $redirectprinters,
         # 
@@ -61,10 +67,12 @@ function New-RdpFile {
         $redirectclipboard,
         # 
         $redirectposdevices,
-        # 
-        $autoreconnection_enabled,
-        # 
-        $authentication_level,
+        # Determines whether the client computer will automatically try to reconnect to the remote computer if the connection is dropped, such as when there's a network connectivity interruption. - 0: Client computer does not automatically try to reconnect - 1: Client computer automatically tries to reconnect
+        [validaterange(0,1)]
+        [int] $autoreconnection_enabled = 1,
+        # Defines the server authentication level settings. - 0: If server authentication fails, connect to the computer without warning (Connect and don’t warn me) - 1: If server authentication fails, don't establish a connection (Don't connect) - 2: If server authentication fails, show a warning and allow me to connect or refuse the connection (Warn me) - 3: No authentication requirement specified.
+        [validaterange(0,2)]
+        [int] $authentication_level = 3,
         # 
         $prompt_for_credentials,
         # 
