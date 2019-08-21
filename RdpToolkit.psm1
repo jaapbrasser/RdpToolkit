0fce82bff611089e2af29a156979d0afbfd4417d
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-21 15:01:05 -0700

    Added import to psm1

--- RdpToolkit.psm1
+++ RdpToolkit.psm1
@@ -1,1 +1,4 @@
-
+[io.directory]::getfiles("$PSScriptRoot\Functions") | ForEach-Object {
+    $function = [io.file]::readalltext($_)
+    $ExecutionContext.InvokeCommand.InvokeScript($function, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
+}
