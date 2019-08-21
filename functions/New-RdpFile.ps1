f4c47e637fdb15b17f0b2936d505a173bf8fb348
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-21 02:55:47 +0200

    All help and default values up to date

--- functions/New-RdpFile.ps1
+++ functions/New-RdpFile.ps1
@@ -1,7 +1,7 @@
 function New-RdpFile {
     param(
         # Path of the RDP file to be created
-        $Path,
+        [string $Path,
         # Determines whether the remote session window appears full screen when you connect to the remote computer by using Remote Desktop Connection. - 1: The remote session will appear in a window - 2: The remote session will appear full screen
         [validaterange(1,2)]
         [int] $screen_mode_id = 2,
@@ -31,34 +31,45 @@ function New-RdpFile {
         # Determines if Remote Desktop Connection will use RDP-efficient multimedia streaming for video playback. - 0: Don't use RDP efficient multimedia streaming for video playback - 1: Use RDP-efficient multimedia streaming for video playback when possible
         [validaterange(0,1)]
         [int] $videoplaybackmode = 1,
-        # 
-        $connection_type,
+        # Specifies pre-defined performance settings for the Remote Desktop session. 1 - Modem (56 Kbps). 2 - Low-speed broadband (256 Kbps - 2 Mbps). 3 - Satellite (2 Mbps - 16 Mbps with high latency). 4 - High-speed broadband (2 Mbps - 10 Mbps). 5 - WAN (10 Mbps or higher with high latency). 6 - LAN (10 Mbps or higher). 7 - Automatic bandwidth detection. Requires bandwidthautodetect. By itself, this setting does nothing. When selected in the RDC GUI, this option changes several performance related settings (themes, animation, font smoothing, etcetera). These separate settings always overrule the connection type setting.
+        [validaterange(1,7)]
+        [int] $connection_type = 7,
         # Determines whether or not to use automatic network bandwidth detection. Requires the option bandwidthautodetect to be set and correlates with connection type 7. - 0: Don't use automatic network bandwidth detection - 1: Use automatic network bandwidth detection
         [validaterange(0,1)]
         [int] $networkautodetect = 1,
         # Determines whether automatic network type detection is enabled - 0: Disable automatic network type detection - 1: Enable automatic network type detection
         [validaterange(0,1)]
         [int] $bandwidthautodetect = 1,
-        # 
-        $displayconnectionbar,
-        # 
-        $enableworkspacereconnect,
-        # 
-        $disable_wallpaper,
-        # 
-        $allow_font_smoothing,
-        # 
-        $allow_desktop_composition,
-        # 
-        $disable_full_window_drag,
-        # 
-        $disable_menu_anims,
-        # 
-        $disable_themes,
-        # 
-        $disable_cursor_setting,
-        # 
-        $bitmapcachepersistenable,
+        # Determines whether the connection bar appears when you are in full screen mode. 0 - Do not show the connection bar. 1 - Show the connection bar.
+        [validaterange(0,1)]
+        [int] $displayconnectionbar = 1,
+        # Determines whether workspace reconnection is enabled. 0 - Disabled 1 - Enabled
+        [validaterange(0,1)]
+        [int] $enableworkspacereconnect = 0,
+        # Determines whether the desktop background is displayed in the remote session. 0 - Display the wallpaper. 1 - Do not show any wallpaper.
+        [validaterange(0,1)]
+        [int] $disable_wallpaper = 1,
+        # Determines whether font smoothing may be used in the remote session. 0 - Disable font smoothing in the remote session. 1 - Font smoothing is permitted.
+        [validaterange(0,1)]
+        [int] $allow_font_smoothing = 0,
+        # Determines whether desktop composition (needed for Aero) is permitted when you log on to the remote computer. 0 - Disable desktop composition in the remote session. 1 - Desktop composition is permitted.
+        [validaterange(0,1)]
+        [int] $allow_desktop_composition = 0,
+        # Determines whether window content is displayed when you drag the window to a new location. 0 - Show the contents of the window while dragging. 1 - Show an outline of the window while dragging.
+        [validaterange(0,1)]
+        [int] $disable_full_window_drag = 1,
+        # Determines whether menus and windows can be displayed with animation effects in the remote session. 0 - Menu and window animation is permitted. 1 - No menu and window animation.
+        [validaterange(0,1)]
+        [int] $disable_menu_anims = 1,
+        # Determines whether themes are permitted when you log on to the remote computer. 0 - Themes are permitted. 1 - Disable theme in the remote session.
+        [validaterange(0,1)]
+        [int] $disable_themes = 1,
+        # Whether remote cursor settings are active in the session 0 - Disabled 1 - Enabled
+        [validaterange(0,1)]
+        [int] $disable_cursor_setting = 0,
+        # Determines whether bitmaps are cached on the local computer (disk-based cache). Bitmap caching can improve the performance of your remote session. 0 - Do not cache bitmaps. 1 - Cache bitmaps.
+        [validaterange(0,1)]
+        [int] $bitmapcachepersistenable,
         # This setting specifies the name or IP address of the remote computer that you want to connect to. A valid computer name, IPv4 address, or IPv6 address.
         [string] $full_address,
         # Determines whether the local or remote machine plays audio. - 0: Play sounds on local computer (Play on this computer) - 1: Play sounds on remote computer (Play on remote computer) - 2: Do not play sounds (Do not play)
@@ -67,8 +78,9 @@ function New-RdpFile {
         # Determines whether printers configured on the client computer will be redirected and available in the remote session when you connect to a remote computer by using Remote Desktop Connection. - 0: The printers on the local computer are not available in the remote session - 1: The printers on the local computer are available in the remote session
         [validaterange(0,1)]
         [int] $redirectprinters = 1,
-        # 
-        $redirectcomports,
+        # Determines whether the COM (serial) ports on the client computer will be redirected and available in the remote session. 0 - The COM ports on the local computer are not available in the remote session. 1 - The COM ports on the local computer are available in the remote session.
+        [validaterange(0,1)]
+        [int] $redirectcomports = 0,
         # Determines whether smart card devices on the client computer will be redirected and available in the remote session when you connect to a remote computer. - 0: The smart card device on the local computer is not available in the remote session - 1: The smart card device on the local computer is available in the remote session
         [validaterange(0,1)]
         [int] $redirectsmartcards = 1,
@@ -111,14 +123,10 @@ function New-RdpFile {
         # Determines whether a user's credentials are saved and used for both the RD Gateway and the remote computer. - 0: Remote session will not use the same credentials - 1: Remote session will use the same credentials
         [validaterange(0,1)]
         [int] $promptcredentialonce = 1,
-        # 
-        $gatewaybrokeringtype,
-        # 
-        $use_redirection_server_name,
-        # 
-        $rdgiskdcproxy,
-        # 
-        $kdcproxyname,
+        # The type of gateway brokering that should be used
+        [int] $gatewaybrokeringtype = 0,
+        # Use Redirection server name. 0 = No, 1 = Yes
+        [int] $use_redirection_server_name = 0,
         # Determines which local disk drives on the client computer will be redirected and available in the remote session. No value specified: don't redirect any drives * : Redirect all disk drives, including drives that are connected later DynamicDrives: redirect any drives that are connected later The drive and labels for one or more drives: redirect the specified drive(s)
         [string] $drivestoredirect
         # 
