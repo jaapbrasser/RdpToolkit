c8f566fc8ac7304dbdd9830d182d82bbf69f7384
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-21 15:03:34 -0700

    Renamed variable

--- RdpToolkit.psm1
+++ RdpToolkit.psm1
@@ -1,4 +1,4 @@
 [io.directory]::getfiles("$PSScriptRoot\Functions") | ForEach-Object {
-    $function = [io.file]::readalltext($_)
-    $ExecutionContext.InvokeCommand.InvokeScript($function, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
+    $functioncontent = [io.file]::readalltext($_)
+    $ExecutionContext.InvokeCommand.InvokeScript($functioncontent, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
 }
