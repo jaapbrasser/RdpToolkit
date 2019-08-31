5aada78a469ceee18f6102a783a147136aaae559
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-30 17:20:00 -0700

    Added code

--- functions/Get-RdpFile.ps1
+++ functions/Get-RdpFile.ps1
@@ -7,4 +7,17 @@ function Get-RdpFile {
         [parameter(Mandatory)]
         [string] $Path
     )
+    
+    Get-Content -LiteralPath $Path | ForEach-Object -Begin {
+        $HashObject = [ordered]@{}
+    } -Process {
+        $CurrentLine = $_.split(':')
+        if ('i' -eq $CurrentLine[1]) {
+            $HashObject.Add($CurrentLine[0], [int]$CurrentLine[2])
+        } elseif ('s' -eq $CurrentLine[1]) {
+            $HashObject.Add($CurrentLine[0], [int]$CurrentLine[2])
+        }
+    } -End {
+        [pscustomobject]$HashObject
+    }
 }
