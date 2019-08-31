92c37d80a08807abc459df081f214cb8703b5322
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-30 17:28:17 -0700

    Added process block

--- functions/Get-RdpFile.ps1
+++ functions/Get-RdpFile.ps1
@@ -4,20 +4,22 @@ function Get-RdpFile {
     [cmdletbinding()]
     param(
         # Path of the RDP file to be created
-        [parameter(Mandatory)]
+        [parameter(Mandatory=$true)]
         [string] $Path
     )
     
-    Get-Content -LiteralPath $Path | ForEach-Object -Begin {
-        $HashObject = [ordered]@{}
-    } -Process {
-        $CurrentLine = $_.split(':')
-        if ('i' -eq $CurrentLine[1]) {
-            $HashObject.Add($CurrentLine[0], [int]$CurrentLine[2])
-        } elseif ('s' -eq $CurrentLine[1]) {
-            $HashObject.Add($CurrentLine[0], [string]$CurrentLine[2])
+    process {
+        Get-Content -LiteralPath $Path | ForEach-Object -Begin {
+            $HashObject = [ordered]@{}
+        } -Process {
+            $CurrentLine = $_.split(':')
+            if ('i' -eq $CurrentLine[1]) {
+                $HashObject.Add($CurrentLine[0], [int]$CurrentLine[2])
+            } elseif ('s' -eq $CurrentLine[1]) {
+                $HashObject.Add($CurrentLine[0], [string]$CurrentLine[2])
+            }
+        } -End {
+            [pscustomobject]$HashObject
         }
-    } -End {
-        [pscustomobject]$HashObject
     }
 }
