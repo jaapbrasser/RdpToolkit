f62fa531e1c5ac5c7b8333087e2f216696149ca3
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-30 17:07:55 -0700

    Created param block

--- functions/Get-RdpFile.ps1
+++ functions/Get-RdpFile.ps1
@@ -1,3 +1,10 @@
 function Get-RdpFile {
-    
+<#
+#>
+    [cmdletbinding()]
+    param(
+        # Path of the RDP file to be created
+        [parameter(Mandatory)]
+        [string] $Path
+    )
 }
