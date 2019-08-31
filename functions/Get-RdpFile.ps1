b98776b1469e14960a1440fda6d085e1533f1a3d
Jaap Brasser <noreply@jaapbrasser.com>
2019-08-30 17:39:46 -0700

    Added comment-based help

--- functions/Get-RdpFile.ps1
+++ functions/Get-RdpFile.ps1
@@ -1,6 +1,31 @@
 function Get-RdpFile {
 <#
+.SYNOPSIS
+Get the .RDP file contents 
+    
+.DESCRIPTION
+This function is used to programmatically gather information from .rdp files, which can be used to create or update existing .Rdp files
+
+.NOTES   
+Name:        Get-RubrikFile
+Module:      RdpToolkit
+Author:      Jaap Brasser
+DateCreated: 2019-08-30
+DateUpdated: 2019-08-30
+Version:     1.0.0
+Blog:        https://www.jaapbrasser.com
+
+.LINK
+https://www.github.com/jaapbrasser/rdptoolkit
+
+.EXAMPLE
+Get-RdpFile -Path $home/server01.rdp
+
+Description
+-----------
+Get structured objects back from the .rdp files
 #>
+
     [cmdletbinding()]
     param(
         # Path of the RDP file to be created
